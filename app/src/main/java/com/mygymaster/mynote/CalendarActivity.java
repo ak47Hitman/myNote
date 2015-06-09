4758703e7dd01df42b0fc5c350a09fc036ef9c86
ak47Hitman <aka47Hitman@gmail.com>
2015-06-09 08:41:22 +0500

    Create activity calendar and insert notes
    Create activity calendar and another activity for calendar. Work
    version.

--- app/src/main/java/com/mygymaster/mynote/CalendarActivity.java
+++ app/src/main/java/com/mygymaster/mynote/CalendarActivity.java
@@ -1,19 +1,30 @@
 package com.mygymaster.mynote;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 /**
  * Created by logvinov on 30.04.2015.
  */
 public class CalendarActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_view);
+        GridView dayOfweekGridView = (GridView)findViewById(R.id.dayOfWeekGridView);
+        GridView numberOfWeekGridView = (GridView)findViewById(R.id.numberOfWeekGridView);
 
-        setContentView(R.layout.calendar);
+        final String[] days = {"  ", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"};
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<>(this,
+                R.layout.day_calendar_item, days);
+        dayOfweekGridView.setAdapter(mAdapter);
+        ArrayAdapter<String> mAdapter2 = new ArrayAdapter<>(this,
+                R.layout.day_calendar_item, days);
+        numberOfWeekGridView.setAdapter(mAdapter2);
     }
     
     public void onNoteButtonClick(View view) {
